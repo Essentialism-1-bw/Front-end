01977d8e947c20d52e9f5d6db115d4f4028038e6
DustinG98 <thedustingraham@gmail.com>
2019-12-18 21:42:19 -0700

    Styling for the form to match design file

--- essentialism/src/components/SignInForm.js
+++ essentialism/src/components/SignInForm.js
@@ -3,59 +3,82 @@ import { withFormik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
 import { TextField } from 'formik-material-ui';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
     card: {
-      width: "30%",
-      padding: 25,
+      marginTop: "10%",
+      width: "25%",
+      height: "50vh",
+      padding: 10,
       display: "flex",
-      justifyContent: "center",
-      alignItems: "center"
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      flexDirection: "column",
+      textAlign: 'left'
     },
     input: {
         marginBottom: 20,
         width: "100%"
     },
     title: {
-        marginBottom: 20,
-        color: '#001763',
+        color: '#000',
+        fontSize: '1.4rem',
         fontWeight: 700,
     },
-    error: {
-        color: "red",
+    signup: {
+        color: '#7932FF'
+    },
+    signin: {
+        marginTop: 30
     }
   });
 
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#7932FF"
+        }
+    }
+})
 
 const SignInForm = ({ values, errors, touched, status }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
+            <div style={{ width: '55%' }}>
+                <Typography className={classes.title}>Sign In</Typography>
+                <p style={{ fontSize: '.8rem' }}>New User? <a href="#" className={classes.signup}>Sign Up Now!</a></p>
+            </div>
             <Form>
                 <div className={classes.input}>
-                    <Field type="text" name="username" placeholder="Username" component={TextField}/>
+                    <Field type="email" name="email" placeholder="Email Address" component={TextField} fullWidth/>
                 </div>
                 <div className={classes.input}>
                     <Field type="password" name="password" placeholder="Password" component={TextField} />
                 </div>
-                <button type="submit">Sign In</button>
+                <ThemeProvider theme={theme}>
+                    <Button className={classes.signin} type="submit" variant="contained" color="primary">Sign In</Button>
+                </ThemeProvider>
             </Form>
         </Card>
     )
 }
 
 const FormikSignInForm = withFormik({
-    mapPropsToValues({ username, password }) {
+    mapPropsToValues({ email, password }) {
         return {
-            username: username || "",
+            email: email || "",
             password: password || "",
         }
     },
     validationSchema: Yup.object().shape({
-        username: Yup.string()
-            .min(8, "Username must be at least 8 characters long")
+        email: Yup.string()
+            .email("Email not valid")
             .required(),
         password: Yup.string()
             .min(8, "Password must be at least 8 characters long")
