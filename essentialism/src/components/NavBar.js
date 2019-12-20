2a3beb8cbd0d50fdf58912a8edf7b527132922c3
Jessica <Jessica@users-iMac.local>
2019-12-19 22:21:09 -0500

    removed sign up from nav

--- essentialism/src/components/NavBar.js
+++ essentialism/src/components/NavBar.js
@@ -6,7 +6,6 @@ const NavBar = () => {
         <div>
             <Link to="/">Home</Link>
             <Link to="/login">Sign In</Link>
-            <Link to="/signup">Create Account</Link>
             <Link to="/dashboard">Dashboard</Link>
         </div>
     )
